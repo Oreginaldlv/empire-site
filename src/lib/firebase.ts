a5325ad7f54b172b8a90c8ce37cc7fd703ed2858
Oreginald Inc LLC (Oreginald Credit) <oindustriesinc@gmail.com>
2025-07-27 15:22:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,4 @@
-
-import { initializeApp, getApps, getApp } from 'firebase/app';
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
